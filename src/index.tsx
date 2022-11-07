207eaca450bb5b0ef726e1fbd23a484301527d9c
신영민 <rightones@outlook.com>
2022-11-07 16:04:31 +0900

    Add PWA

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -13,6 +14,8 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>,
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
